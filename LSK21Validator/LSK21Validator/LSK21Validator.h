41dbd7b18197f999ad774ed616550372c89cf44c
Tarun Kumar <tarun.kumar@tvsmotor.com>
2024-09-05 16:02:18 +0530

    add framework

---
+++ LSK21Validator/LSK21Validator/LSK21Validator.h
@@ -0,0 +1,18 @@
+//
+//  LSK21Validator.h
+//  LSK21Validator
+//
+//  Created by macOS Sonoma on 05/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LSK21Validator.
+FOUNDATION_EXPORT double LSK21ValidatorVersionNumber;
+
+//! Project version string for LSK21Validator.
+FOUNDATION_EXPORT const unsigned char LSK21ValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSK21Validator/PublicHeader.h>
+
+
